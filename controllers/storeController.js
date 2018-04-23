40df2e21e12f9ea06de709343e4450fe101df62b
Charlotte Spencer <charlottelaspencer@gmail.com>
2018-04-23 10:21:07 +0100

    feat: list and edit stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose')
 const Store = mongoose.model('Store')
 
-exports.homePage = (req, res) => {
-  res.render('index')
-}
-
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' })
 }
@@ -14,4 +10,24 @@ exports.createStore = async (req, res) => {
 
   req.flash("success", `Successfully created <strong>${store.name}</strong>. Care to leave a review?`)
   res.redirect(`/store/${store.slug}`)
+}
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find()
+  res.render('stores', { title: 'Stores', stores })
+}
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id })
+  res.render('editStore', { title: `Editing ${store.name}`, store})
+}
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate(
+    { _id: req.params.id},
+    req.body,
+    { new: true, runValidators: true }
+  ).exec()
+  req.flash('success', `Succesfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`)
+  res.redirect(`/stores/${store._id}/edit`)
 }
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,13 @@ const router = express.Router()
 const storeController = require('../controllers/storeController')
 const { catchErrors } = require('../handlers/errorHandlers')
 
-router.get('/', storeController.homePage)
+router.get('/', catchErrors(storeController.getStores))
+
+router.get('/stores', catchErrors(storeController.getStores))
+router.get('/stores/:id/edit', catchErrors(storeController.editStore))
+
 router.get('/add', storeController.addStore)
 router.post('/add', catchErrors(storeController.createStore))
+router.post('/add/:id', catchErrors(storeController.updateStore))
 
 module.exports = router
